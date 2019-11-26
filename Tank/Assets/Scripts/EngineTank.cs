e375f91094ed3397bcd68d1a07c24291a2b795ab
cakesonia <prsofa1223@gmail.com>
2019-11-26 22:15:56 +0200

    Added code after review

--- Tank/Assets/Scripts/EngineTank.cs
+++ Tank/Assets/Scripts/EngineTank.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class EngineTank : MonoBehaviour {
 
     //the tank's speed
-    float MoveSpeed = 1.5f;
+    [SerializeField] float MoveSpeed = 1.5f;
     //the tank's rotate speed
     float RotateSpeed = 20;
-    //the tank's current speed
-    float CurrentSpeed = 0;
-    //the tank's speed
+    
+    
     int SpeedNum = 0;
     //the tank movement's component
     Rigidbody TankEngine;
@@ -20,25 +19,12 @@ public class EngineTank : MonoBehaviour {
     //ствол
     public GameObject StartBarrel;
 
-    //a function for tank's moving
-    void MoveTank()
-    {
-        //where a tank will move
-        Vector3 Move = transform.forward * CurrentSpeed * MoveSpeed * Time.deltaTime;
-        
-        //get the current tank's position
-        Vector3 Position = TankEngine.position + Move;
-        
-        //move a tank
-        TankEngine.MovePosition(Position);
-
-    }
-
     //the tank's rotation
     void RotateTank()
     {
         //calculate the rotation
         float R = Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
+        
         
         //create new angle of tank's rotation
         Quaternion RotateAngle = Quaternion.Euler(0, R, 0);
@@ -50,8 +36,16 @@ public class EngineTank : MonoBehaviour {
         TankEngine.MoveRotation(CurrentAngle);
     }
 
+    void Move(KeyCode Key, int direction)
+    {
+        if (Input.GetKey(Key))
+        {
+            TankEngine.velocity = transform.rotation * Vector3.forward * MoveSpeed * direction;
+        }
+    }
+
     //increase the tank's speed
-    void UpSpeed()
+        void UpSpeed()
     {
         if ((SpeedNum + 1) < 2) SpeedNum++;
     }
@@ -62,22 +56,6 @@ public class EngineTank : MonoBehaviour {
         if ((SpeedNum - 1) > -2) SpeedNum--;
     }
 
-    void Transmission()
-    {
-        switch (SpeedNum)
-        {
-            case -1: CurrentSpeed = -1; RotateSpeed = 25; break; //reverse
-            case 0: CurrentSpeed = 0; RotateSpeed = 20; break; //neutral
-            case 1: CurrentSpeed = 1f; RotateSpeed = 25; break; //forward
-        }
-    }
-
-    //to know that a tank "crashed" in something
-    void OnCollisionEnter(Collision collision)
-    {
-        SpeedNum = 0;
-    }
-
     void Fire()
     {
         if (Input.GetButtonUp("Fire4"))
@@ -85,23 +63,16 @@ public class EngineTank : MonoBehaviour {
             //the current coordinates of barrel for the bullet creating
             Vector3 MazzlePoint = StartBarrel.transform.position;
             Quaternion MazzleRotation = StartBarrel.transform.rotation;
-            GameObject Bullet_for_fire = Instantiate(Bullet, MazzlePoint, MazzleRotation) as GameObject;
+            GameObject BulletForFire = Instantiate(Bullet, MazzlePoint, MazzleRotation) as GameObject;
 
             //get a component
-            Rigidbody Run = Bullet_for_fire.GetComponent<Rigidbody>();
+            Rigidbody Run = BulletForFire.GetComponent<Rigidbody>();
+            
             //speed up the bullet
-            Run.AddForce(Bullet_for_fire.transform.forward * 30, ForceMode.Impulse);
+            Run.AddForce(BulletForFire.transform.forward * 30, ForceMode.Impulse);
 
-            Destroy(Bullet_for_fire, 1);
+            Destroy(BulletForFire, 1);
         }
-    }
-
-    void FixedUpdate()
-    {
-        Fire();
-        Transmission();
-        MoveTank();
-        RotateTank();
     }
 
     // Use this for initialization
@@ -112,13 +83,10 @@ public class EngineTank : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        // remember what user has pressed
-        float Axis = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonUp("Vertical"))
-        {
-            if (Axis > 0) UpSpeed();
-            if (Axis < 0) DownSpeed();
-        }
+        Fire();
+        Move(KeyCode.UpArrow, 1);
+        Move(KeyCode.DownArrow, -1);
+        RotateTank();
     }
 }
